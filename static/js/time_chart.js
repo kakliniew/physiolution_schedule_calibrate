c1bef0be63d70220451f845525c50a7f83e10b82
Karoll <238896@student.pwr.edu.pl>
2019-11-23 09:18:13 +0100

    changed something

--- static/js/time_chart.js
+++ static/js/time_chart.js
@@ -1,4 +1,5 @@
 // Global parameters:
+let selectedPoint = null;
 let currentChannel = localStorage.getItem("current.channel");
 if (currentChannel !== null) {
     currentChannel = parseInt(currentChannel);
@@ -6,23 +7,65 @@ if (currentChannel !== null) {
     currentChannel = 0;
 }
 
-// function assertChannel() {
-//     if (currentChannel === null) {
-//         alert("Najpierw musisz wybrać kanal")
-//         return true;
-//     }
-//     return false;
-// }
-
 // do not resize the chart canvas when its container does (keep at 600x400px)
 Chart.defaults.global.responsive = true;
-var tbl = document.getElementById("tblData");
-var timeFormat = 'HH:mm';
-var colCount = tbl.rows[0].cells.length;
-var rowCount = tbl.rows.length;
+
+let table = document.getElementById("tblData");
+let indexRow = table.rows[0];
+let valueRow = table.rows[1];
+let timeRow = table.rows[2];
+
+function getTd(row, index) {
+    if (row.cells.length <= index) {
+        let element = document.createElement("td");
+        row.appendChild(element);
+    }
+    return row.cells[index];
+}
+
+function getInput(element) {
+    if (element.firstChild === null) {
+        let input = generateInput();
+        element.appendChild(input);
+        return input;
+    }
+    if (element.firstChild.tagName === "input") {
+        return element.firstChild;
+    } else {
+        element.firstChild.remove();
+        return getInput(element);
+    }
+}
+
+function setIndex(index, value) {
+    let element = getTd(indexRow, index);
+    element.className = index - 1 === selectedPoint ? "active" : "";
+    element.textContent = value;
+}
+
+function setValue(index, value) {
+    let element = getTd(valueRow, index);
+    element.className = index - 1 === selectedPoint ? "active" : "";
+    let input = getInput(element);
+    input.value = value;
+}
+
+function setTime(index, value) {
+    let element = getTd(timeRow, index);
+    element.className = index - 1 === selectedPoint ? "active" : "";
+    let input = getInput(element);
+    input.value = value;
+}
+
+function removeExcess(row) {
+    let data = getData();
+    while (data.length < row.cells.length - 1) {
+        row.cells[row.cells.length - 1].remove();
+    }
+}
 
 function timeFromMinutes(minutes) {
-    return newDateString(Math.floor(minutes / 60), minutes % 60);
+    return newDateString(Math.floor(minutes / 60), Math.floor(minutes % 60));
 }
 
 function newDateString(hours, minutes) {
@@ -34,72 +77,110 @@ function parseTimeToMinutes(value) {
     return (parseInt(split[0]) * 60) + parseInt(split[1]);
 }
 
-function updateIndex() {
-    for (var i = 1; i < tbl.rows[0].cells.length; i++) {
-        tbl.rows[0].cells[i].innerHTML = i;
-    }
+function insertPointAfter(index) {
+    let schedule = getSchedule();
+    let point = schedule[index];
+    schedule.splice(index + 1, 0, {
+        y: point.y,
+        x: point.x
+    });
+
+    setData(convertSchedule(schedule));
+    updateTable();
 }
 
-function getLastPoint() {
-    var data = myChart.data.datasets[0].data;
-    if (data.length > 0) {
-        return data[data.length - 1];
-    }
-    return null;
+function setData(data) {
+    myChart.data.datasets[0].data = data;
+    updateData();
 }
 
-function insertPointBefore(index) {
-    var data = myChart.data.datasets[0].data;
-    if (data.length <= index) {
-        return;
-    }
-    var oldPoint = data[index];
-    var beforePoint = data.length >= index ? data[index - 1] : undefined;
-    var newPoint = {
-        x: oldPoint.x - 10,
-        y: oldPoint.y
-    };
-    if (beforePoint !== undefined && beforePoint.x > newPoint.x) {
-        newPoint.x = beforePoint.x;
-    }
-    if (oldPoint.x < 0) {
-        oldPoint.x = 0;
-    }
-    insertSpecificPointBefore(index, newPoint);
+function getData() {
+    return myChart.data.datasets[0].data;
 }
 
-function insertSpecificPointBefore(index, newPoint) {
-    var data = myChart.data.datasets[0].data;
-    if (data.length <= index) {
-        return;
-    }
-
-    insertBefore(0, index + 1, $("<td>" + index + "</td>")[0]);
-    insertBefore(1, index + 1, $("<td>").append($(generateInput(newPoint.y, false)))[0]);
-    insertBefore(2, index + 1, $("<td>").append($(generateInput(timeFromMinutes(newPoint.x), true)))[0]);
-
-    data.splice(index, 0, newPoint);
-
-    updateIndex();
+function getSchedule() {
+    return convertData(getData());
 }
 
-function insertBefore(row, index, element) {
-    let currentElement = tbl.rows[row].cells[index];
-    currentElement.parentNode.insertBefore(element, currentElement);
+function updateData() {
+    myChart.update();
+}
+
+function clearData() {
+    setData([]);
+    updateTable();
+}
+
+function convertSchedule(schedule) {
+    let data = [];
+    var actualTime = 0;
+    for (var i = 0; i < schedule.length; i++) {
+        actualTime += schedule[i].x;
+        data.push({
+            x: actualTime,
+            y: schedule[i].y
+        });
+    }
+    return data;
+}
+
+function convertData(data) {
+    let schedule = [];
+    let lastValue = 0;
+    for (let i = 0; i < data.length; i++) {
+        let point = data[i];
+        schedule.push({
+            y: point.y,
+            x: point.x - lastValue
+        });
+        lastValue = point.x;
+    }
+    return schedule;
+}
+
+function updateChart() {
+    console.log("UPDATE CHART");
+    let tableData = parseData();
+    setData(convertSchedule(tableData));
+    saveChannel();
+}
+
+function updateTable() {
+    console.log("UPDATE TABLE");
+
+    let schedule = convertData(getData());
+    for (let i = 0; i < schedule.length; i++) {
+        let point = schedule[i];
+        setIndex(i + 1, i + 1);
+        setValue(i + 1, point.y);
+        setTime(i + 1, timeFromMinutes(point.x));
+    }
+    removeExcess(indexRow);
+    removeExcess(valueRow);
+    removeExcess(timeRow);
+
+    document.getElementById("managePoints").className = selectedPoint !== null ? "active" : "";
+
+    saveChannel();
+}
+
+function updatePoint(i) {
+    let data = getData();
+
+    let lastValue = i > 0 ? data[i - 1].x : 0;
+    let point = data[i];
+    setIndex(i + 1, i + 1);
+    setValue(i + 1, point.y);
+    setTime(i + 1, timeFromMinutes(point.x - lastValue));
+    saveChannel();
 }
 
 function removePoint(index) {
-    tbl.rows[2].cells[index + 1].remove();
-    tbl.rows[1].cells[index + 1].remove();
-    tbl.rows[0].cells[index + 1].remove();
-
-    myChart.data.datasets[0].data.splice(index, 1);
-    updateIndex();
-}
-
-function setPoint(index, point) {
-    tbl.rows[2].cells[index + 1].children[0].value = timeFromMinutes(Math.round(point.x));
-    tbl.rows[1].cells[index + 1].children[0].value = point.y;
+    var schedule = getSchedule();
+    schedule.splice(index, 1);
+    setData(convertSchedule(schedule));
+    updateTable();
+    updateChart();
 }
 
 // define the chart data
@@ -146,7 +227,7 @@ document.getElementById("start").addEventListener("click", function () {
             updateButtons(data.alive);
         },
         contentType: "application/json",
-        data: JSON.stringify({schedule: transformData()}),
+        data: JSON.stringify({schedule: getSchedule()}),
     });
 });
 
@@ -162,8 +243,27 @@ document.getElementById("stop").addEventListener("click", function () {
     });
 });
 document.getElementById("add").addEventListener("click", function () {
-    addValue();
-    reloadChart()
+    let schedule = getSchedule();
+    let x = schedule.length > 0 ? schedule[schedule.length - 1].x : 7;
+    let y = schedule.length > 0 ? schedule[schedule.length - 1].y : 10;
+    schedule.push({
+        y: y,
+        x: x
+    });
+    setData(convertSchedule(schedule));
+    updateTable();
+});
+
+document.getElementById("addAfterPoint").addEventListener("click", function () {
+    if (selectedPoint !== null) {
+        insertPointAfter(selectedPoint);
+    }
+});
+
+document.getElementById("removePoint").addEventListener("click", function () {
+    if (selectedPoint !== null) {
+        removePoint(selectedPoint);
+    }
 });
 
 document.getElementById("loadTemp").addEventListener("click", function () {
@@ -250,15 +350,15 @@ var myChart = new Chart(ctx, {
             }
             if (index > 0 && data[index - 1].x > value.x) {
                 value.x = data[index - 1].x;
-                return;
             } else if (index < data.length - 1 && data[index + 1].x < value.x) {
                 value.x = data[index + 1].x;
-                return;
             }
-            setPoint(index, value);
+            updatePoint(index);
+            updateChart();
         },
         onDragEnd: function (e, datasetIndex, index, value) {
-            reloadChart();
+            updatePoint(index);
+            updateChart();
         },
         scales: {
             yAxes: [{
@@ -317,20 +417,24 @@ holder.onclick = function (evt) {
     console.log(evt);
     var activePoint = myChart.getElementAtEvent(evt);
     if (activePoint.length > 0 && evt.ctrlKey) {
-        insertPointBefore(activePoint[0]._index);
-        reloadChart();
+        insertPointAfter(activePoint[0]._index);
     } else if (activePoint.length > 0 && evt.shiftKey) {
         removePoint(activePoint[0]._index);
-        reloadChart();
     } else if (activePoint.length > 0) {
-        console.log(activePoint);
-        console.log('x:' + activePoint[0]._view.x);
-        console.log('maxWidth: ' + activePoint[0]._xScale.maxWidth);
-        console.log('y: ' + activePoint[0]._view.y);
-        console.log('index: ' + activePoint[0]._index);
-        pointSelected.innerHTML = 'Point selected... index: ' + (activePoint[0]._index + 1);
+        selectPoint(activePoint[0]._index);
     }
 };
+
+function selectPoint(index) {
+    pointSelected.innerHTML = 'Point selected... index: ' + (index + 1);
+
+    let element = indexRow.cells[index + 1];
+    let parentTable = element.parentNode.parentNode.parentNode.parentNode;
+    parentTable.scrollLeft = element.offsetLeft - (parentTable.clientWidth / 2);
+
+    selectedPoint = index;
+    updateTable();
+}
 
 function startAnimation(element, time, maxTime, offset) {
     if (processStop === false) {
@@ -349,74 +453,15 @@ function stopAnimation() {
     processStop = true;
 }
 
-function generateInput(value, time) {
+function generateInput() {
     let input = document.createElement("input");
     input.className = "time-input";
     input.size = "1";
     input.type = "text";
-    input.value = value;
     input.onchange = function () {
-        reloadChart();
+        updateChart();
     };
     return input;
-}
-
-function addValue() {
-
-    $("#tblData").find('tr').each(function () {
-        var trow = $(this);
-        if (trow.index() === 0) {
-            trow.append("<td>" + cellLength() + "</td>");
-        } else {
-
-            var input;
-            if (trow.index() === 1) {
-                input = generateInput(cellLength() > 2 ? "7.00" : "0", false);
-            } else {
-                input = generateInput(cellLength() > 2 ? timeFromMinutes(parseTimeToMinutes(tbl.rows[2].cells[cellLength() - 2].children[0].value) + 10) : "00:00", true);
-            }
-            trow.append($("<td>").append($(input)));
-        }
-    });
-}
-
-function cellLength() {
-    return tbl.rows[0].cells.length;
-}
-
-function reloadChart() {
-    let data = parseData();
-    let oldData = myChart.data.datasets[0].data;
-    for (var i = 0; i < data.length; i++) {
-        if (i > 0 && data[i - 1].x > data[i].x) {
-            data[i].x = oldData[i].x;
-            setPoint(i, data[i]);
-        } else if (i < data.length - 1 && data[i + 1].x < data[i].x) {
-            data[i].x = oldData.length <= i ? 0 : oldData[i].x;
-            setPoint(i, data[i]);
-        }
-    }
-    myChart.data.datasets[0].data = data;
-    myChart.update();
-
-    saveChannel();
-}
-
-function clearChart() {
-    console.log("clear chart");
-    var row1 = document.getElementById("firstRow");
-    var row2 = document.getElementById("secondRow");
-    var row3 = document.getElementById("thirdRow");
-
-    var size = cellLength();
-    for (var i = 1; i < size; i++) {
-        row1.deleteCell(1);
-        row2.deleteCell(1);
-        row3.deleteCell(1);
-    }
-
-    myChart.data.datasets[0].data = [];
-    myChart.update();
 }
 
 function createChannels() {
@@ -480,18 +525,10 @@ function loadTemplateFromFile(selectedTemplate) {
             return {x: x.interval, y: x.pH};
         });
 
-        clearChart();
+        clearData();
 
-        var actualTime = 0;
-        for (var i = 0; i < schedule.length; i++) {
-            addValue();
-            actualTime += schedule[i].x;
-            setPoint(i, {
-                x: actualTime,
-                y: schedule[i].y
-            });
-        }
-        reloadChart();
+        setData(convertSchedule(schedule));
+        updateTable();
     });
 }
 
@@ -512,7 +549,7 @@ function saveTemplate() {
         url: "/saveTemplate",
         type: "POST",
         contentType: "application/json",
-        data: JSON.stringify({schedule: transformData(), name: templateName, description: templateDescription})
+        data: JSON.stringify({schedule: getSchedule(), name: templateName, description: templateDescription})
     });
     loadListOfTemplates();
 }
@@ -520,20 +557,12 @@ function saveTemplate() {
 function loadChannel(index) {
     let item = localStorage.getItem("channel." + index);
 
-    clearChart();
+    clearData();
     if (!item) {
         return;
     }
-    let channel = JSON.parse(item);
-
-    for (var i = 0; i < channel.length; i++) {
-        addValue();
-        setPoint(i, {
-            x: channel[i].x,
-            y: channel[i].y
-        });
-    }
-    reloadChart();
+    setData(convertSchedule(JSON.parse(item)));
+    updateTable();
 }
 
 function saveChannel() {
@@ -565,7 +594,6 @@ function onStart() {
         updateButtons(data.alive);
     });
     createChannels();
-    // loadChartFromFile();
     selectChannel(currentChannel);
     loadListOfTemplates();
 }
@@ -574,26 +602,13 @@ function parseData() {
 
     var schedule = [];
 
-    var n = tbl.rows[0].cells.length;
+    var n = table.rows[0].cells.length;
     for (var i = 1; i < n; i++) {
         let point = {
-            x: parseTimeToMinutes(tbl.rows[2].cells[i].children[0].value),
-            y: parseFloat(tbl.rows[1].cells[i].children[0].value)
+            x: parseTimeToMinutes(table.rows[2].cells[i].children[0].value),
+            y: parseFloat(table.rows[1].cells[i].children[0].value)
         };
         schedule.push(point);
     }
     return schedule;
-}
-
-function transformData() {
-    let data = parseData();
-
-    let lastValue = 0;
-    for (var i = 0; i < data.length; i++) {
-        let currentValue = data[i].x;
-        data[i].x = currentValue - lastValue;
-        lastValue = currentValue;
-    }
-
-    return data;
 }
